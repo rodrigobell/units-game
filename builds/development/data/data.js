0dba4c16213a1217a64301111921a5c873af8067
Rodrigo Bell <rbell594@gmail.com>
2016-11-16 09:48:20 -0800

    Moved data.js from js folder to data folder

---
+++ builds/development/data/data.js
@@ -0,0 +1,261 @@
+var imperial = {
+	length: {
+		inch: {
+			unit: "Inch",
+			article: "an",
+			data: [{
+				imgUrl: "images/imperial/length/inch1.jpg", 
+				caption: "Vertical length of the human eye", 
+				info: "~0.94 inches",
+				correct: true},{
+				imgUrl: "images/imperial/length/inch2.jpg", 
+				caption: "Diameter of a golf ball", 
+				info: "1.68 inches",
+				correct: false},{
+				imgUrl: "images/imperial/length/inch3.jpg", 
+				caption: "Diameter of a beer bottle cap", 
+				info: "1.17 inches",
+				correct: false},{
+				imgUrl: "images/imperial/length/inch4.jpg", 
+				caption: "Height of a business card", 
+				info: "2 inches",
+				correct: false}]
+		}, /*inch*/ 
+		foot: {
+			unit: "Foot",
+			article: "an",
+			data: [{
+				imgUrl: "images/imperial/length/foot/shoe.jpg", 
+				caption: "Men's 14 size shoe", 
+				info: "12 Inches",
+				correct: true},{
+				imgUrl: "images/imperial/length/foot/banana1.jpg", 
+				caption: "Length of a banana", 
+				info: "7 to 8 Inches",
+				correct: false},{
+				imgUrl: "images/imperial/length/foot/soccer_ball.jpg", 
+				caption: "Diameter of a soccer ball",  
+				info: "8.65 Inches",
+				correct: false},{
+				imgUrl: "images/imperial/length/foot/labrador1.jpg", 
+				caption: "Height of a labrador",  
+				info: "22 to 24 Inches",
+				correct: false}]
+		}, /*foot*/
+		yard: {
+			unit: "Yard",
+			article: "a",
+			data: [{
+				imgUrl: "images/imperial/length/yard/dollar.png", 
+				caption: "Six one dollar bills side by side", 
+				info: "~1.02 Yards", 
+				correct: true}, {
+				imgUrl: "images/imperial/length/yard/billiards.jpg", 
+				caption: "Width of a billiards table", 
+				info: "1.5 Yards", 
+				correct: false}, {
+				imgUrl: "images/imperial/length/yard/sedan.jpg", 
+				caption: "Width of a mid-sized sedan", 
+				info: "~2 Yards", 
+				correct: false},{
+				imgUrl: "images/imperial/length/yard/woman.jpg", 
+				caption: "Average woman's height", 
+				info: "~1.7 Yards <br> (or 5 ft 4 in)", 
+				correct: false}]
+		}, /*yard*/ 
+		mile: {
+			unit: "Mile",
+			article: "a",
+			data: [{
+				imgUrl: "images/imperial/length/mile/road.jpeg", 
+				caption: "Distance travelled at 60 mph in a minute", 
+				info: "~1.08 Miles", 
+				correct: true},{
+				imgUrl: "images/imperial/length/mile/city_block.jpeg", 
+				caption: "Ten city blocks", 
+				info: "~0.6 Miles <br> (320 ft per block)", 
+				correct: false},{
+				imgUrl: "images/imperial/length/mile/sniper.jpg", 
+				caption: "Longest recorded sniper kill", 
+				info: "~1.53 Miles", 
+				correct: false},{
+				imgUrl: "images/imperial/length/mile/walking.jpeg", 
+				caption: "Human walking speed per hour", 
+				info: "~3.1 Miles", 
+				correct: false}]
+		} /*mile*/
+	}, /*length*/
+	volume: {
+		fluidOunce: {
+			unit: "Fluid Ounce",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},{
+				imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*fluid ounce*/
+		cup: {
+			unit: "Cup",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},{
+				imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*cup*/
+		pint: {
+			unit: "Pint",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},{
+				imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*pint*/
+		quart: {
+			unit: "Quart",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},{
+				imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*quart*/
+		gallon: {
+			unit: "Gallon",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},
+				{imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		} /*gallon*/
+	} /*volume*/
+	mass: {
+		ounce: {
+			unit: "Ounce",
+			article: "an",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},
+				{imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*ounce*/
+		pound: {
+			unit: "Pound",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},
+				{imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}]
+		}, /*pound*/
+		ton: {
+			unit: "Ton",
+			article: "a",
+			data: [{
+				imgUrl: "images/other/placeholder1.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: true},
+				{imgUrl: "images/other/placeholder2.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder3.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false},{
+				imgUrl: "images/other/placeholder4.jpg", 
+				caption: "Placeholder Text", 
+				info: "placeholder text",
+				correct: false}
+			]
+		} /*ton*/
+	} /*mass*/
+} /*imperial*/
